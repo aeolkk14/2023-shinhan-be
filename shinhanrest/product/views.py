8a4ae9b8d62964f29ff3167f2ca29645cd2551e1
aeolkk14 <jeonej0514@gmail.com>
2023-01-19 09:55:14 +0900

    feat: serializers.py create

--- shinhanrest/product/views.py
+++ shinhanrest/product/views.py
@@ -1,16 +1,32 @@
 from rest_framework import generics, mixins
 from .models import Product
+from .serializers import ProductSerializer
 
-class ProductListView(mixins.ListModelMixin, generics.GenericAPIView): 
+class ProductListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin, 
+    generics.GenericAPIView
+): 
     # python 여러 클래스 상속 가능
     # base: generics.GenericAPIView
-    # 기능: mixins.ListModelMixin
+    # 기능: mixins.
+
+    serializer_class = ProductSerializer
     
     def get_queryset(self): 
-        return Product.objects.all().order_by('id')
+        products = Product.objects.all()
+
+        if 'name' in self.request.query_params:
+            name = self.request.query_params.get('name')
+            products=products.filter(name__contains=name)
+
+        return products.order_by('id')
 
     def get(self, request, *args, **kwargs):
         # Queryset
         # Serialize
         # return Response
         return self.list(request, args, kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
